a60c09b1dfe076cf960125f15973441953c5bb86
zero <ahmadabdulaziz380@gmail.com>
2023-11-29 12:54:26 +0700

    fixing

---
+++ ahmad/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-2x font-bold">Hallo Gibahtech</h1>
+    </div>
+  );
+}
--- index.js
+++ index.js
@@ -1,39 +1,57 @@
 #!/usr/bin/env node
 
-import inquirer from 'inquirer';
-import * as fs from 'fs';
-import { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import createDirectoryContents from './createDirectoryContents.js';
+import chalk from "chalk";
+import gradient from "gradient-string";
+import chalkAnimation from "chalk-animation";
+import figlet from "figlet";
+import { createSpinner } from "nanospinner";
+import inquirer from "inquirer";
+import * as fs from "fs";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import createDirectoryContents from "./createDirectoryContents.js";
 const CURR_DIR = process.cwd();
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const CHOICES = fs.readdirSync(`${__dirname}/templates`);
 
+function winner() {
+  console.clear();
+  figlet(`Congrats \n $ 1 , 0 0 0 , 0 0 0`, (err, data) => {
+    console.log(gradient.pastel.multiline(data) + "\n");
+  });
+}
+
+const spinner = createSpinner("").start();
+
+setTimeout(() => {
+  spinner.success();
+}, 1000);
+
 const QUESTIONS = [
   {
-    name: 'project-choice',
-    type: 'list',
-    message: 'What project template would you like to generate?',
+    name: "project-choice",
+    type: "list",
+    message: "What project template would you like to generate?",
     choices: CHOICES,
   },
   {
-    name: 'project-name',
-    type: 'input',
-    message: 'Project name:',
+    name: "project-name",
+    type: "input",
+    message: "Project name:",
     validate: function (input) {
-      if (/^([A-Za-z\-\\_\d])+$/.test(input)) return true;
-      else return 'Project name may only include letters, numbers, underscores and hashes.';
+      if (/^([A-Za-z\-\\_\d])+$/.test(input))
+        return spinner.stop({ text: "Done!", color: "magenta" }), true;
+      else return spinner.error({ text: "Error!", mark: ":(" });
     },
   },
 ];
 
-inquirer.prompt(QUESTIONS).then(answers => {
-  const projectChoice = answers['project-choice'];
-  const projectName = answers['project-name'];
+inquirer.prompt(QUESTIONS).then((answers) => {
+  const projectChoice = answers["project-choice"];
+  const projectName = answers["project-name"];
   const templatePath = `${__dirname}/templates/${projectChoice}`;
 
   fs.mkdirSync(`${CURR_DIR}/${projectName}`);
-
   createDirectoryContents(templatePath, projectName);
 });
